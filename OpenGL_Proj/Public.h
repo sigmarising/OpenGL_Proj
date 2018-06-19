67573d7aae45fa475d947448108d98693b237df5
Yun Zhang <sigmarising@hotmail.com>
2018-06-19 14:50:06 +0800

    tab to space

--- OpenGL_Proj/Public.h
+++ OpenGL_Proj/Public.h
@@ -12,25 +12,25 @@ using namespace std;
 // ------------ 宏定义部分 ------------
 
 // 窗口定义
-#define WIN_NAME	"跳舞的房间"
-#define WIN_WIDTH	900
-#define WIN_HEIGHT	600
+#define WIN_NAME    "跳舞的房间"
+#define WIN_WIDTH   900
+#define WIN_HEIGHT  600
 
 // 自定义纹理大小定义
-#define imageWidth	10
+#define imageWidth  10
 #define imageHeight 10
 
 
 // ------------ 全局变量部分 ------------
 
 // 纹理
-extern GLubyte	image[imageHeight][imageWidth][4];
+extern GLubyte  image[imageHeight][imageWidth][4];
 
 // 视角
-extern GLfloat xrot;
-extern GLfloat yrot;
+extern GLfloat  xrot;
+extern GLfloat  yrot;
 
 // 鼠标相关
-extern bool		mouseLeftDown;
-extern float	mouseX;
-extern float	mouseY;
+extern bool     mouseLeftDown;
+extern float    mouseX;
+extern float    mouseY;
